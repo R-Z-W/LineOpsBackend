a2fe79f993d9791ea8a944eac5ecf36fd69bb4ab
R-Z-W <whiteleyzenki@gmail.com>
2024-12-27 17:47:26 +1100

    Add Car Model
    Create car model and setup schema

---
+++ src/models/CarModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+
+const CarSchema = new mongoose.Schema({
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  vin: { type: String, required: true, unique: true },
+  licensePlate: { type: String, required: true },
+  color: { type: String, required: true },
+  engineType: { type: String },
+  transmissionType: { type: String },
+  mileage: { type: Number },
+  fuelType: { type: String },
+  price: { type: Number },
+  condition: { type: String },
+  seatingCapacity: { type: Number },
+  drivetrain: { type: String },
+  status: { type: String, default: "For Sale" },
+  location: { type: String },
+  boughtDate: { type: Date },
+  soldDate: { type: Date },
+});
+
+// Auto-increment carId
+CarSchema.plugin(AutoIncrement, { inc_field: "carId" });
+
+const Car = mongoose.model("Car", CarSchema);
+module.exports = { Car };
