ebb74230bfa579887cee02ece95cfbe7bc7105c6
R-Z-W <whiteleyzenki@gmail.com>
2024-12-27 17:53:35 +1100

    Add Authentication Routes
    Create authentication route (for testing purposes)

--- src/server.js
+++ src/server.js
@@ -24,3 +24,34 @@ app.use(cors(corsOptions));
 app.get("/api/health", (req, res) => { //accessed with backendurl/api/health
 	res.status(200).json({ message: "Backend is working!" });
 });
+
+// Authentication Routes
+app.post("/signup", async (req, res) => {
+	const { username, password } = req.body;
+  
+	if (!username || !password) {
+	  return res.status(400).json({
+		message: "Incorrect or missing sign-up credentials provided.",
+	  });
+	}
+  
+	// Password strength (8 characters, 1 number)
+	const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+	if (!passwordRegex.test(password)) {
+		return res.status(400).json({
+			message: "Password must be at least 8 characters long and contain at least one number.",
+		});
+	}
+
+	const hashedPassword = await bcrypt.hash(password, 10);  // Hash password before saving
+	let newUser = await User.create({ username, password: hashedPassword });
+	let newJwt = generateJWT(newUser.id, newUser.username);
+  
+	res.json({
+	  jwt: newJwt,
+	  user: {
+		id: newUser.id,
+		username: newUser.username,
+	  },
+	});
+});
